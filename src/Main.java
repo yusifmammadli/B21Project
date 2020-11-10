ca7311ffca011187342b6643aaf012b650c1f520
Joseph <yusif.mammadli.98@gmail.com>
2020-11-10 12:14:04 -0500

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("what's up freak bitches");
+        System.out.println("what's up");
+        // is good at intellij
     }
 }
