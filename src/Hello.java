d3407634aa2b3ae7b62513320242a2b8d68e9c98
Joseph <yusif.mammadli.98@gmail.com>
2020-11-10 12:51:47 -0500

    initial commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        //
+        // more stuff
+        // commit dialog finally
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("what's up");
         // is good at intellij
+        //
     }
 }
